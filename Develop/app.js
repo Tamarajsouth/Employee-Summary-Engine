33f1bb510695a263318da3bf1064b6c998aa6160
tamara south <tjeansouth@gmail.com>
2020-04-16 18:06:26 -0400

    employee.js class and constructor added, modified the consts in app.js

--- Develop/app.js
+++ Develop/app.js
@@ -1,15 +1,21 @@
-// const Manager = require("./lib/Manager");
-// const Engineer = require("./lib/Engineer");
-// const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-// const OUTPUT_DIR = path.resolve(__dirname, "output");
-// const outputPath = path.join(OUTPUT_DIR, "team.html");
-// const writeFileAsync = util.promisify(fs.writeFile);
-// const htmlRenderer = require("./htmlRenderer")
 const htmlRenderer = require("./lib/htmlRenderer");
 
+// const Manager = require("./lib/Manager");
+// const Engineer = require("./lib/Engineer");
+// const Intern = require("./lib/Intern");
+// const inquirer = require("inquirer");
+// const path = require("path");
+// const fs = require("fs");
+// const OUTPUT_DIR = path.resolve(__dirname, "output");
+// const outputPath = path.join(OUTPUT_DIR, "team.html");
+// const render = require("./lib/htmlRenderer");
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
 function promptUser() {
     return inquirer.prompt ([
         {
@@ -96,18 +102,18 @@ function promptUser() {
     ])
 }
     async function init(){
-        console.log("you did it!")
+        console.log("Questions generated")
         try {
             const answers = await promptUser();
-
+// render function passing in the array of employee objects
+// generates a block of html code for each employee
             const html = htmlRenderer(answers);
             await writeFileAsync("index.html", html);
-            console.log("wrote to html");
+            console.log("Success - rendered to .html");
         } catch (err) {
             console.log(err);
         }
     }
-
 
 init();
 
@@ -118,28 +124,8 @@ init();
 // above) and pass in an array containing all employee objects; the `render` function will
 // generate and return a block of HTML including templated divs for each employee!
 
-// function writeToFile(fileName, data) {
-//     fs.writeFile(fileName, data, (err) => {
-//       if (err) {
-//         console.log(err);
-//       } else {
-//         console.log("Success! HTML file generated");
-//       }
-//     });
-//   }
-
 // After you have your html, you're now ready to create an HTML file using the HTML
 // returned from the `render` function. Now write it to a file named `team.html` in the
 // `output` folder. You can use the variable `outputPath` above target this location.
 // Hint: you may need to check if the `output` folder exists and create it if it
 // does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -10,4 +10,18 @@ class Employee {
         this.id = id;
         this.email = email;
     };
-}
+
+    getName() {
+        return this.name;
+    };
+
+    getId() {
+        return this.id;
+    };
+
+    getEmail() {
+        return this.email;
+    };
+};
+
+module.exports = Employee;
